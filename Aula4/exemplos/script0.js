b4a1c0177c076e496903334cfae7f9e80268ddbf
lopespt <guilhermewachs@gmail.com>
2019-02-20 10:13:48 -0300

    Aula3 e 4

---
+++ Aula4/exemplos/script0.js
@@ -0,0 +1,36 @@
+function movable(x) {
+    this.myEl = document.getElementById(x);
+    this.myEl.addEventListener("click", function(){
+        console.log("teste");
+    });
+}
+
+function criaCirculo(x, y) {
+    var s = '<circle cx="' +
+        x + '" cy="' + y + '" r="5" fill="red"/>';
+    return s;
+}
+
+var tela = document.getElementById("tela");
+for (var i = 0; i < 10; i++) {
+    var x = i;
+    var y = x * x / 10.0 - 5 * x + 100;
+    tela.innerHTML += criaCirculo(x * 10, y);
+}
+
+var bolinha = document.getElementById("bolinha");
+
+var bx = 0;
+
+function anima() {
+    bolinha.setAttribute("cx", bx);
+    bx = bx + 5;
+    bx = bx % 800;
+}
+window.setInterval(
+    anima,
+    50
+);
+
+new movable("bolinha");
+
